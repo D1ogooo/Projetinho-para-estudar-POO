1a9b6c5c993a4b38851df9c007ffbaf717a26096
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2024-11-17 18:03:09 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Pratos = require("../controllers/PratosController");
 
 routes.post("/pratos/create", (req, res) => Pratos.create(req, res)); // c
 routes.get("/pratos/list", (req, res) => Pratos.list(req, res)); // r
-routes.put("/pratos/update", (req, res) => Pratos.update(req, res));
+routes.put("/pratos/update", (req, res) => Pratos.update(req, res)); // u
 routes.delete("/pratos/delete/:id", (req, res) => Pratos.delete(req, res)); // d
 
 module.exports = routes;
